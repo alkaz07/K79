d4b0fbe66437adc5508775eb7e303106d7b0983e
alkaz07 <alkaz07@yandex.ru>
2022-12-27 21:29:56 +0300

    fixed: setXY before matrix initialization in constructor

--- src/Main.java
+++ src/Main.java
@@ -9,47 +9,20 @@ public class Main {
 
         Scanner scanner = new Scanner(new File("input.txt"));
         String proga = scanner.nextLine(); // читаем файл
-        int l= proga.length();
-        //System.out.println(l);
-        char[] cmdArray= proga.toCharArray();
-
-        int[][] matrix = new int[101][101]; // создаем матрицу;
-        int counter = 0; // создаем счетчик шагов робота;
-        int result=-1;
+        int l = proga.length();
+        char[] cmdArray = proga.toCharArray();
+        int n = 101;
+        int[][] matrix = new int[n][n]; // создаем матрицу;
+        int result = -1;
         // задать координаты;
-        Robot rob = new Robot(50, 50, 0, matrix);
-        int x = 50;
-        int y = 50;
-        int direction = 0; //направление
-        matrix[x][y] = 1;
+        Robot rob = new Robot(n/2, n/2, 0, matrix);
+
         // запускаем робота;
-        for (char cmd : cmdArray ) {
-            if(cmd == 'S'){
-                counter++;
-                if(direction == 0)
-                    y++;
-                if(direction == 90)
-                    x++;
-                if(direction == 180)
-                    y--;
-                if(direction == 270)
-                    x--;
-                if(matrix[x][y]==1){
-                    result=counter;
-                    break;
-                }
-                else
-                    matrix[x][y]=1;
-            }
-            if(cmd == 'R'){
-                direction = direction+90;
-                if(direction == 360)
-                    direction=0;
-            }
-            if(cmd == 'L'){
-                direction = direction-90;
-                if(direction == -90)
-                    direction=270;
+        for (char cmd : cmdArray) {
+            rob.executeCommand(cmd);
+            if ( matrix[ rob.getX() ][ rob.getY() ] > 1) {
+                result = rob.getStepCounter();
+                break;
             }
         }
 
@@ -59,3 +32,5 @@ public class Main {
 
     }
 }
+
+
